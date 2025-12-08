8fc01938ec0329dcb2fb9f83661689bbe9e6662a
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-12-08 13:16:29 +0700

    complete models mapping database

---
+++ product-service/src/main/java/com/ecom/product_service/model/Category.java
@@ -0,0 +1,27 @@
+package com.ecom.product_service.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Category extends BaseEntity {
+    
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
+    @Column(name = "parent_id")
+    private Long parentId;
+}
