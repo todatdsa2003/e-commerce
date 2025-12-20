ab91a461f429354ee6c1579e5e36417c68a9c656
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-12-20 13:20:46 +0700

    fix: remove interceptor causing locale resolution error

--- product-service/src/main/java/com/ecom/product_service/config/WebConfig.java
+++ product-service/src/main/java/com/ecom/product_service/config/WebConfig.java
@@ -9,11 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -45,19 +43,6 @@ public class WebConfig implements WebMvcConfigurer {
         AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
         resolver.setDefaultLocale(new Locale("vi")); // Mac dinh tieng Viet
         return resolver;
-    }
-
-    // Cho phep doi ngon ngu bang query param ?lang=en hoac ?lang=vi
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-        interceptor.setParamName("lang");
-        return interceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
     }
 
     @Override
