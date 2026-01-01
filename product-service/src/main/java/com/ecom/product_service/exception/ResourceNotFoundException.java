f49eadf24b8715728807d7ab792ae4b1ca7ee354
todatdsa2003 <todat.dsa.2003@gmail.com>
2026-01-02 01:11:57 +0700

    chore(user): remove product-service from user branch

--- product-service/src/main/java/com/ecom/product_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ecom.product_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- product-service/src/main/java/com/ecom/product_service/service/ProductDescriptionService.java
+++
@@ -1,9 +0,0 @@
-package com.ecom.product_service.service;
-
-import com.ecom.product_service.dto.ProductDescriptionRequest;
-import com.ecom.product_service.response.ProductDescriptionResponse;
-
-public interface ProductDescriptionService {
-    
-    ProductDescriptionResponse generateDescription(ProductDescriptionRequest request);
-}
