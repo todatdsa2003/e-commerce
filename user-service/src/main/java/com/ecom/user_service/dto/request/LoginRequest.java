84426fa5f6b1964fb0740a28fc4594e149308c73
todatdsa2003 <todat.dsa.2003@gmail.com>
2026-01-11 17:39:35 +0700

    feat(auth): add automatic whitespace trimming for login and register requests(not working)

--- user-service/src/main/java/com/ecom/user_service/dto/request/LoginRequest.java
+++ user-service/src/main/java/com/ecom/user_service/dto/request/LoginRequest.java
@@ -3,10 +3,10 @@ package com.ecom.user_service.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
@@ -17,12 +17,4 @@ public class LoginRequest {
 
     @NotBlank(message = "Password is required")
     private String password;
-
-    public void setEmail(String email) {
-        this.email = email != null ? email.trim() : null;
-    }
-
-    public void setPassword(String password) {
-        this.password = password != null ? password.trim() : null;
-    }
 }
